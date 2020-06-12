c2e1c656b5220d9081eef519a61a5140ca5fd639
STIVEN-J4 <65369672+STIVEN-J4@users.noreply.github.com>
2020-06-11 20:01:04 -0500

    uml

---
+++ UML1.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uml1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Julio jimenez
+ */
+public class UML1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Scanner objeto = new Scanner(System.in);
+        System.out.println("Estudio experimental y predicción de rendimiento y emisión en un "
+                + "motor SI que utiliza combustible alternativo con red neuronal artificial:");
+        System.out.println("\n   Ingrese el objeto seleccionado del paper:");
+        String clp =objeto.nextLine();
+
+        System.out.println("\n   Ingrese Atributo 1 :");
+        String atri1 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 2 :");
+        String atri2 =objeto.nextLine();
+        System.out.println("\n   Ingrese Atributo 3 :");
+        String atri3 =objeto.nextLine();
+
+        String atri4 =objeto.nextLine();
+        System.out.println("\n   Ingrese Servicios seleccionado del paper:");
+        System.out.println("\n   Ingrese Servicio 1 :");
+        String servi1 =objeto.nextLine();
+        System.out.println("\n   Ingrese Servicio 2 :");
+        String servi2 =objeto.nextLine();
+        System.out.println("\n   Ingrese Servicio 3 :");
+        String servi3 =objeto.nextLine();
+
+
+    }
+
+}
